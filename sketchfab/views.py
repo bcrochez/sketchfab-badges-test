3dd185e35d720c635c5b8e36d78985aa6ef4ff98
bcrochez <bastien.crochez@gmail.com>
2016-10-17 22:43:46 +0200

    Add some views

--- sketchfab/views.py
+++ sketchfab/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout, login, authenticate
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Welcome to sketchfab')
+    return render(request, 'sketchfab/index.html')
+
+
+def login_view(request):
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        #else:
+            # show error
+    return render(request, 'sketchfab/login.html')
+
+
+def logged_out(request):
+    logout(request)
+    return render(request, 'sketchfab/logout.html')
