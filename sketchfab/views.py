f4826bc3d66481de30a13cbef0f701e1534fa9c1
bcrochez <bastien.crochez@gmail.com>
2016-10-18 14:52:37 +0200

    Add upload model view

--- sketchfab/views.py
+++ sketchfab/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Model3D, SketchfabUser
 
@@ -26,3 +27,13 @@ def user_view(request, user_id):
     return render(request, 'sketchfab/user.html', {
         'user': user
     })
+
+
+@login_required(login_url='sketchfab:login')
+def upload(request):
+    if request.POST:
+        model_name = request.POST['name']
+        model = Model3D(user=request.user, name=model_name)
+        model.save()
+        return redirect('/sketchfab/user/'+str(request.user.id))
+    return render(request, 'sketchfab/upload.html')
