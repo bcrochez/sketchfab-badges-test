119cdb7b5012f0de08eeee612f1edf27158de9a5
bcrochez <bastien.crochez@gmail.com>
2016-10-18 10:56:24 +0200

    Add model and user views

--- sketchfab/urls.py
+++ sketchfab/urls.py
@@ -7,5 +7,7 @@ app_name = "sketchfab"
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', auth_views.login, {'template_name': 'sketchfab/login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'template_name': 'sketchfab/logout.html'}, name='logout')
+    url(r'^logout/$', auth_views.logout, {'template_name': 'sketchfab/logout.html'}, name='logout'),
+    url(r'^model/(?P<model_id>[0-9]+)/$', views.model_view, name='model'),
+    url(r'^user/(?P<user_id>[0-9]+)/$', views.user_view, name='user')
 ]
--- sketchfab/views.py
+++ sketchfab/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Model3D, SketchfabUser
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'sketchfab/index.html')
+    models = Model3D.objects.all()
+    return render(request, 'sketchfab/index.html', {
+        'models': models
+    })
+
+
+def model_view(request, model_id):
+    model = get_object_or_404(Model3D, pk=model_id)
+    model.views += 1
+    model.save()
+    return render(request, 'sketchfab/model.html', {
+        'model': model
+    })
+
+
+def user_view(request, user_id):
+    user = get_object_or_404(SketchfabUser, pk=user_id)
+    return render(request, 'sketchfab/user.html', {
+        'user': user
+    })
