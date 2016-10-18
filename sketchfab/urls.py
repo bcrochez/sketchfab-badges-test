d62eacacb71b7c3f2fcfc4466fc9e2314d046431
bcrochez <bastien.crochez@gmail.com>
2016-10-18 10:22:14 +0200

    Update views and templates

--- sketchfab/urls.py
+++ sketchfab/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
+app_name = "sketchfab"
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login/$', views.login_view, name='login'),
-    url(r'^logout/$', views.logged_out, name='logout')
+    url(r'^login/$', auth_views.login, {'template_name': 'sketchfab/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'sketchfab/logout.html'}, name='logout')
 ]
--- sketchfab/views.py
+++ sketchfab/views.py
@@ -1,28 +1,7 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import logout, login, authenticate
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'sketchfab/index.html')
-
-
-def login_view(request):
-    if request.user.is_authenticated :
-        return redirect('index')
-    if request.POST:
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('index')
-        #else:
-            # show error
-    return render(request, 'sketchfab/login.html')
-
-
-def logged_out(request):
-    logout(request)
-    return render(request, 'sketchfab/logout.html')
