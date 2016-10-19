8668aa8e15e0fbc0dae0e359b245b49dbff8dffb
bcrochez <bastien.crochez@gmail.com>
2016-10-19 10:03:05 +0200

    Add badge model

--- sketchfab/models.py
+++ sketchfab/models.py
@@ -7,19 +7,26 @@ from datetime import datetime
 
 
 class SketchfabUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_signin = models.DateTimeField(default=datetime.now)
 
     def __str__(self):
         return "User " + self.user.username
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    date_signin = models.DateTimeField(default=datetime.now)
-
 
 class Model3D(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    views = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name + " by " + self.user.username
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
-    views = models.IntegerField(default=0)
+
+class Badge(models.Model):
+    users = models.ManyToManyField(User)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "Badge " + self.title
